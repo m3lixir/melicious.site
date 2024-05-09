d3ac7f50f5fbb273fe707f269f30d2dc7c1e5163
Melisa K. Savich <melisasavich@gmail.com>
2024-05-09 05:59:30 -0400

    update sumfetch.ts

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -1,46 +1,82 @@
 import config from '../../../config.json';
 
-const sumfetch = async (args: string[]): Promise<string> => {
-  if (config.ascii === 'cveinnt') {
-    return `                                                  
-             @@@@@@@@@@@@@                   sumfetch: summary display
-        @@@@               @@@@             -----------
-      @@                       @@            ABOUT
-    @@                           @@          ${config.name}
-  @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-@@                                   @@     -----------
-@@             .@@@@@@@@@@.          @@      CONTACT 
- @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-     @@@                        @@@         -----------
-        @@@                  @@@ @@          DONATE 
-         @|  @@@@@@@@@@@@@@@@   @@           <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-         @|                      @@          <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+const sumfetch = async () => {
+  const output = `
+=======================================================================
+|                          OFFICIAL USE ONLY                          |
+|                                                                     |
+|                          ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                           |
+|                       ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄                        |
+|                     ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄                      |
+|                   ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                     |
+|                  ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓                    |
+|               ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓                 |
+|               ▐▓                                 ▐▓                 |
+|               ▐▓ [ VulnCorp Top Secret Dossier ] ▐▓                 |
+|               ▐▓                                 ▐▓                 |
+|               ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓                 |
+|                  ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓                    |
+|                   ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓                     |
+|                     ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀                      |
+|                       ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                         |
+|                           ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                            |
+|                                                                     |               |
+|                         OFFICIAL USE ONLY                           |
+=======================================================================
+| Terminal ID: 0x1337B33F | Scanning retina... Access Granted!        |
+-----------------------------------------------------------------------
+| Subject Alias:          "MeliciousCode"                             |
+| Coordinates:            Secured under Protocol Alpha-9              |
+| Academy Trail:                                                      |
+| - Infiltrated University of Michigan, Ann Arbor, Michigan,          |
+|   Dual B.S. in Computer Science and Women's Studies, 2018.          |
+|   Specialist in digital combat and socio-psychological manipulation.|
+| - Advanced tactical training at New York University, Brooklyn, New  |
+|   York:                                                             |
+|   M.S. Computer Science, 2020.                                      |
+|   Thesis: "TAINT-DRIVEN FIRMWARE FUZZING OF EMBEDDED SYSTEMS"       |
+|   Pioneered novel penetration strategies in embedded system security.|
+-----------------------------------------------------------------------
+| Mission Credentials:                                                |
+| - Critical Skills Master's Program Fellow, Sandia National          |
+|   Laboratories (2018-2020). Spearheaded innovations in cyber        |
+|   modeling, simulation of high-consequence networks, and strategic  |
+|   deployment of ICS/SCADA systems.                                  |
+| - Research & Development Intern, Sandia National Laboratories       |
+|   (2017-2018). Developed a cutting-edge drone simulation program for|
+|   cyber disruption and constructed virtual honeypots to lure and    |
+|   analyze enemy tactics.                                            |
+| - Operational aide in academic environments (2016-2018), honed      |
+|   tactical knowledge transfer skills, and orchestrated high-stakes  |
+|   exam logistics for large scale operations.                        |
+-----------------------------------------------------------------------
+| Last Known Deployment:                                              |
+| - October 2023: Commanded high-security cyber warfare missions with |
+|   classified outcomes. Asset has since initiated dark mode for      |
+|   operational security and advanced strategic recalibration.        |
+|   Current location: Untraceable.                                    |
+-----------------------------------------------------------------------
+| Skill Arsenal:                                                      |
+| - Proficient in an array of programming languages including Bash,   |
+|   C++, Python; expert in cyber defense operations and penetration   |
+|   testing frameworks. Mastery in both offensive and defensive       |
+|   cyber tactics.                                                    |
+| - Linguistic fluency in English and Macedonian, with operational    |
+|   proficiency in French, enhancing covert communication capabilities.|
+-----------------------------------------------------------------------
+| Recruitment Priority: [##########] 100%                             |
+| Status: Critical Asset. Engage immediately to capitalize on unique  |
+| capabilities and secure loyalty for high-impact missions.           |
+-----------------------------------------------------------------------
+| Directive: Utilize all necessary resources to ensure re-engagement. |
+| Prepare to deploy substantial incentives to retain asset for future |
+| high-stakes operations.                                             |
+=======================================================================
 `;
-  } else {
-    return `
-           ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▄                  sumfetch
-        ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
-      ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
-    ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-▐▓                                 ▐▓       -----------
-▐▓        > L I V E T E R M        ▐▓        CONTACT 
-▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             DONATE 
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀                <u><a href="${config.donate_urls.paypal}" target="_blank">${config.donate_urls.paypal}</a></u>
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                   <u><a href="${config.donate_urls.patreon}" target="_blank">${config.donate_urls.patreon}</a></u>
 
-`;
-  }
-};
+return output };
+
 
 export default sumfetch;
